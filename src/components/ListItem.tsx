747d8b8e18b00cfd7cd2c68a92794e2045049b02
pafiore <pabloandresfiore@gmail.com>
2022-10-03 10:31:05 -0300

    feat(ListItem): add favorite icon

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,5 +1,6 @@
-import React, { memo } from 'react';
-import {Text, Image, StyleSheet, View, TouchableWithoutFeedback} from 'react-native';
+import React, { memo, useState } from 'react';
+import {Text, Image, StyleSheet, View, TouchableWithoutFeedback, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { thousandsAndDecimalSeparatorFormat } from '../utils/NumberUtils';
 import { Item } from '../types';
 
@@ -13,19 +14,37 @@ interface ListItemProps {
 const ListItem = ({item, onPress = () => null}: ListItemProps) => {
 
     // console.log('ITEM ID: ' + item.id);
+
+    const [favorite, setFavorite] = useState<boolean>(false)
+
+    const onPressFavorite = (itemID: number): void => {
+        setFavorite(!favorite)
+        console.log(itemID)
+    }
   
-  return (
-    <TouchableWithoutFeedback onPress={() => onPress(item.id)}>
-      <View style={styles.container}>
-        <Image source={{uri: item.thumbnail}} style={styles.image} />
-        <View style={styles.innerContainer}>
-            <Text style={styles.textTitle} numberOfLines={1}>{item.title}</Text>
-            <Text style={styles.textDescription} numberOfLines={2}>{item.description}</Text>
-            <Text style={styles.textPrice}>${thousandsAndDecimalSeparatorFormat(item.price)}</Text>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+    return (
+        <TouchableWithoutFeedback onPress={() => onPress(item.id)}>
+            <View style={[styles.container, favorite ? {backgroundColor: '#d9fceb'} : null]}>
+                <Image source={{uri: item.thumbnail}} style={styles.image} />
+                <View style={styles.innerContainer}>
+                    
+                    <View style={styles.titleIconContainer}>
+                        <Text style={styles.textTitle} numberOfLines={1}>{item.title}</Text>
+                        {
+                            favorite
+                            ?
+                            <Icon name='heart' style={styles.iconFavoriteSelected} onPress={() => onPressFavorite(item.id)} />
+                            :
+                            <Icon name='heart-o' style={styles.iconFavorite} onPress={() => onPressFavorite(item.id)} />
+                        }
+                    </View>
+
+                    <Text style={styles.textDescription} numberOfLines={1}>{item.description}</Text>
+                    <Text style={styles.textPrice}>${thousandsAndDecimalSeparatorFormat(item.price)}</Text>
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    );
 }
 
 export default memo(ListItem);
@@ -44,6 +63,7 @@ const styles = StyleSheet.create({
     },
     innerContainer:{
       flexDirection:'column',
+      flex:1,
     },
     image:{
       width: 80,
@@ -52,14 +72,18 @@ const styles = StyleSheet.create({
       borderTopLeftRadius: 10,
       borderBottomLeftRadius: 10,
     },
+    titleIconContainer: {
+        flexDirection:'row', 
+        justifyContent: 'space-between', 
+        paddingRight:10
+    },
     textTitle:{
       fontSize:17,
       fontWeight: 'bold',
       color: '#4262fe',
-      paddingRight:95,
     },
     textDescription:{
-      paddingRight:95,
+      paddingRight: 20,
       fontSize:13,
       color: 'gray',
   
@@ -69,4 +93,11 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       color: '#02d474'
     },
+    iconFavorite: {
+        fontSize:25
+    },
+    iconFavoriteSelected: {
+        fontSize:25,
+        color: '#02d474'
+    }
   });
